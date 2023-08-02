b46dae00f945e4a9bd1c10133de153d258ee952f
laasri1990 <laasrii2013@gmail.com>
2023-08-02 16:24:07 -0700

    available item

---
+++ src/gitclass03.java
@@ -0,0 +1,10 @@
+public class gitclass03 {
+    public static void main(String[] args) {
+        boolean produvtavailable=true;
+        if(produvtavailable){
+            System.out.println("yes");
+        }else{
+            System.out.println("yes");
+        }
+    }
+}
